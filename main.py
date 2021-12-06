7e2a06e9c45523aea192feaf8e0fc01e6ac4498c
Adnan <adikhanbloch@gmail.com>
2021-12-06 18:11:57 +0500

    add vaiable parser in the parser.

--- main.py
+++ main.py
@@ -2,19 +2,31 @@ import time
 from parser import Parser
 
 parser = Parser()
+payload = []
 
 def follow(thefile):
     thefile.seek(0,2) # Go to the end of the file
+    global payload
     while True:
         line = thefile.readline()
         if not line:
+            if len(payload) != 0:
+                print(payload)
+                # send(payload)
+                payload = []
             time.sleep(0.1) # Sleep briefly
             continue
         yield line
+
 
 for line in follow(open("test_file", "r")):
     if line =='':
         continue
     line =  line.rstrip().lstrip()
     parsed = parser.parse(line)
+    
+    # temp_log_object = {}
+    # temp_log_object["logType"] = "syslog"
+    # temp_log_object['time'] = parsed['timestamp']
+
     print(parsed)
--- parser.py
+++ parser.py
@@ -1,20 +1,22 @@
 import sys
 from pyparsing import Word, alphas, Suppress, Combine, nums, string, Optional, Regex
 from time import strftime
+from datetime import datetime, date
+import re
 
 class Parser(object):
   def __init__(self):
     ints = Word(nums)
-
+  
     # timestamp
     month = Word(string.ascii_uppercase , string.ascii_lowercase, exact=3)
     day   = ints
-    hour  = Combine(ints + ":" + ints + ":" + ints)
+    time_collect  = Combine(ints + ":" + ints + ":" + ints)
     
-    timestamp = month + day + hour
+    timestamp = month + day + time_collect
 
-    # service_name
-    service_name = Word(alphas + nums + "_" + "-" + ".")
+    # hostname
+    hostname = Word(alphas + nums + "_" + "-" + ".")
 
     # appname
     appname = Word(alphas + "/" + "-" + "_" + ".") + Optional(Suppress("[") + ints + Suppress("]")) + Suppress(":")
@@ -28,14 +30,30 @@ class Parser(object):
   def parse(self, line):
     parsed = self.__pattern.parseString(line)
 
-    payload              = {}
-    payload["timestamp"] = strftime("%Y-%m-%d %H:%M:%S")
-    payload["hostname"]  = parsed[4]
-    payload["appname"]   = parsed[5]
-    payload["pid"]       = parsed[6]
-    try:
-        payload["message"]   = parsed[7]
-    except IndexError:
-        payload["message"] = ''
-    return payload
+    
+    payload = {}
+    payload["timestamp"] = parsed[2] + parsed[1] + "//" + str(datetime.strptime(parsed[0], "%b").month) + "//" + str(date.today().year)
+    payload["hostname"]  = parsed[3]
+    payload["appname"]   = parsed[4]
 
+    if len(parsed) == 7:      
+      payload["pid"]       = parsed[5]
+      payload["message"]   = parsed[6]
+    elif len(parsed) == 6:
+      proccess_parsed_text = parsed[5].replace(re.findall("\[.*?\]", parsed[5])[0] + " ", '') # remove extra content from it
+      list_of_variables = proccess_parsed_text.split(" ")
+      
+      for a_variable in list_of_variables:
+        if '=' in a_variable:
+          list_from_a_variable = a_variable.split("=")
+          payload[list_from_a_variable[0]] = '='.join (map(str, list_from_a_variable[1:]))
+        elif ':' in a_variable:
+          list_from_a_variable = a_variable.split(":")
+          payload[list_from_a_variable[0]] = ':'.join (map(str, list_from_a_variable[1:]))
+
+
+      
+    else:
+      raise Exception("Need to handle this log")
+      
+    return payload
